550156510c8cc0f87e2728d4b20a28b3b3832a01
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2024-11-23 12:04:50 +0200

    updated

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,38 +1,52 @@
 import csv
-
 import pyodbc
 from pymongo import database
 
 
 class DatabaseManager:
-    def __init__(self, driver: str, server: str, username: str, password: str, database: str):
-        connection_string = f"DRIVER={driver};SERVER={server};UID={username};PWD={password};DATABASE={database}"
-        self.connection=pyodbc.connect(connection_string)
-        print("Connection established successfully")
+    def __init__(self, driver: str, server: str, username: str, password: str):
+        self.connection_string = f"DRIVER={driver};SERVER={server};UID={username};PWD={password};DATABASE={username}"
+
 
     def file_to_database(self, path: str) -> None:
         try:
-            cursor = self.connection.cursor()
+            connection = pyodbc.connect(self.connection_string)
+            print("Connection established successfully")
+
+            cursor = connection.cursor()
             with open(path, "r") as file:
                 csv_file = csv.reader(file)
 
                 for row in csv_file:
+                    # validate both entries
                     if len(row) < 2:
                         continue
                     title = row[0]
                     prod_year = row[1]
+
+                    # check that the values are in correct format
+                    if not title or not prod_year.isdigit():
+                        continue
+
                     title_length = len(title)  # Calculate the length of the title
 
+                    # check that there aren't any duplicates
+                    cursor.execute("SELECT COUNT(*) FROM dbo.MediaItems WHERE TITLE = ? AND PROD_YEAR = ?",
+                                   (title, prod_year))
+                    if cursor.fetchone()[0] > 0:
+                        continue
+
                     cursor.execute(
-                        "INSERT INTO MediaItems (TITLE, PROD_YEAR, TITLE_LENGTH) VALUES (?, ?, ?)",
+                        "INSERT INTO dbo.MediaItems (TITLE, PROD_YEAR, TITLE_LENGTH) VALUES (?, ?, ?)",
                         (title, prod_year, title_length)
                     )
-                    self.connection.commit()
+                    print("New entries were successfully inserted")
+                    connection.commit()
         except Exception as e:
             print("Error", e)
         finally:
-            if self.connection:
-                self.connection.close()
+            if connection:
+                connection.close()
 
     def calculate_similarity(self) -> None:
         # TODO
@@ -46,4 +60,7 @@ class DatabaseManager:
         pass    
 
 if __name__ == '__main__':
-    print("hello")
+    dm = DatabaseManager("ODBC Driver 18 for SQL Server", "132.72.64.124", "amirrot", "RPzc9yQh", "amirrot")
+    # dm.file_to_database("C:\\Users\\Rotem\\Documents\\GitHub\\DBM\\hw1\\films.csv")
+    # dm.calculate_similarity()
+    dm.add_summary_items()
